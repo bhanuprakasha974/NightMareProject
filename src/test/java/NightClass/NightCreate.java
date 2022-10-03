0486f37a200e406444619f9e30cdf36ad14d80c7
USER <USER@HP>
2022-10-03 23:57:01 +0530

    night created

---
+++ src/test/java/NightClass/NightCreate.java
@@ -0,0 +1,14 @@
+package NightClass;
+
+import org.testng.annotations.Test;
+
+public class NightCreate
+{
+
+	@Test
+	public void nightCreate()
+	{
+		System.out.println("night ctreated");
+	}
+}
+
